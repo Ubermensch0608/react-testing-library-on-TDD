93b146c904e6a241e0ed834535008deb28a0980a
JongWon <melancholyee28@gmail.com>
2022-09-01 03:54:15 +0900

    Fix: msw handler 반환 오류 해결

--- sundaes-on-demand/src/pages/entry/__tests__/OrderEntry.test.jsx
+++ sundaes-on-demand/src/pages/entry/__tests__/OrderEntry.test.jsx
@@ -6,12 +6,12 @@ import { server } from '../../../mocks/server';
 describe('Order Entry', () => {
   test('스툽 및 토핑 라우터를 다룬다.', async () => {
     server.resetHandlers(
-      rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-        res(ctx.status(500));
-      }),
-      rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
-        res(ctx.status(500));
-      })
+      rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
+        res(ctx.status(500))
+      ),
+      rest.get('http://localhost:3030/toppings', (req, res, ctx) =>
+        res(ctx.status(500))
+      )
     );
 
     render(<OrderEntry />);
